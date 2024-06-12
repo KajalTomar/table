f7ff0be3d5edeb9ad2142e5dc2c96e3d1fdc0105
Kajal Tomar <52635061+KajalTomar@users.noreply.github.com>
2024-06-12 12:17:41 -0500

    Update table.c

--- table.c
+++ table.c
@@ -1,10 +1,4 @@
 // -----------------------------------------------------------------------------------------------
-// NAME: Kajal Tomar
-// STUDENT NUMBER: 7793306
-// COURSE: COMP2160, SECTION: A01
-// INSTRUCTOR: Dr.Mehdi Niknam
-// ASSIGNMENT: 3, QUESTION 1
-//
 // REMARKS: This is my implementation of a table ADT that uses a linked list. 
 // -----------------------------------------------------------------------------------------------
 
